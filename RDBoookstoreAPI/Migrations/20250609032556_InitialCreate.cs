6e1fab9db55fc2b3ef8c6037e15e9338547044a2
rholexxdizon <dizonrholex02@gmail.com>
2025-06-09 14:12:31 +0800

    Migrate database from Azure SQL Server to local SQL Express for testing
    - Updated connection string for local development
    - Applied initial migrations to SQL Express

---
+++ RDBoookstoreAPI/Migrations/20250609032556_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RDBookstoreAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Overview = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Publisher = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Pages = table.Column<int>(type: "int", nullable: false),
+                    Length = table.Column<short>(type: "smallint", nullable: false),
+                    Width = table.Column<short>(type: "smallint", nullable: false),
+                    Height = table.Column<short>(type: "smallint", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PublicationDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SalesRank = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
